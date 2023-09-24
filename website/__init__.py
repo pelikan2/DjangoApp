6cdef5fa3c1002d13c067aa3b7a004938d5cbea5
pelikan2 <pelikan.vladimir24@gmail.com>
2023-09-24 18:25:49 +0200

    database setup

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 # create from website folder python package
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -17,5 +18,10 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/') #
     app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Note
+
+    with app.app_context():
+        db.create_all()
 
     return app
--- website/models.py
+++ website/models.py
@@ -1,6 +1,15 @@
 from . import db #from website folder import db
 from flask_login import UserMixin # helps user log in
+from sqlalchemy.sql import func
 
-# noinspection PyUnresolvedReferences
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 class User(db.Model, UserMixin): #define all column in database
-    id = db.C
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
